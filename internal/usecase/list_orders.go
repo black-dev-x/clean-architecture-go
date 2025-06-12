a4a9fff730d9b15073f4c733e8e85e39785bcca1
Thiago <thiago.dev.x@gmail.com>
2025-06-11 21:34:12 -0300

    update

---
+++ internal/usecase/list_orders.go
@@ -0,0 +1,39 @@
+package usecase
+
+import (
+	"github.com/black-dev-x/clean-architecture-go/internal/entity"
+)
+
+type OrderDTO struct {
+	ID         string  `json:"id"`
+	Price      float64 `json:"price"`
+	Tax        float64 `json:"tax"`
+	FinalPrice float64 `json:"final_price"`
+}
+
+type ListOrdersUseCase struct {
+	OrderRepository entity.OrderRepositoryInterface
+}
+
+func NewListOrdersUseCase(
+	OrderRepository entity.OrderRepositoryInterface,
+) *ListOrdersUseCase {
+	return &ListOrdersUseCase{
+		OrderRepository: OrderRepository,
+	}
+}
+
+func (c *ListOrdersUseCase) Execute() (*[]OrderDTO, error) {
+	orders, err := c.OrderRepository.FindAll()
+
+	var dtos []OrderDTO
+	for _, order := range *orders {
+		dtos = append(dtos, OrderDTO{
+			ID:         order.ID,
+			Price:      order.Price,
+			Tax:        order.Tax,
+			FinalPrice: order.FinalPrice,
+		})
+	}
+	return &dtos, err
+}
